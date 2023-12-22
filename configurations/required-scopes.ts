2e25c6e4d9efe752892d0aba22d4c8087a35feb2
Bryan Wood <itanex@users.noreply.github.com>
2023-12-22 09:00:31 -0800

    Update readme with content (#41)

---
+++ configurations/required-scopes.ts
@@ -0,0 +1,17 @@
+const requiredScopes = [
+    'analytics:read:games',
+    'bits:read',
+    'channel:manage:broadcast',
+    'channel:manage:redemptions',
+    'channel:moderate',
+    'channel:read:hype_train',
+    'channel:read:redemptions',
+    'channel:read:subscriptions',
+    'chat:edit',
+    'chat:read',
+    'moderation:read',
+    'moderator:manage:announcements',
+    'moderator:read:followers',
+    'user:read:follows',
+    'user:read:subscriptions',
+];
