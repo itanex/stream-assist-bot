83a99a52ac44def58175b9008564af3ca2d2f7c4
Bryan Wood <itanex@gmail.com>
2024-01-16 08:09:03 -0800

    Add styled connection display

--- bot/overlay/index.script.js
+++ bot/overlay/index.script.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 const notification = document.getElementById("notification");
+const socketStatus = document.getElementById("socket-status");
 const audio = new Audio();
 const retryInterval = 2000;
 const maxRetryCount = 60;
@@ -8,62 +10,68 @@ let retryCount = 0;
 let ws = null;
 
 function onCloseHanlder(event) {
-  console.log(`Web Socket: Connection closed`, event);
+    console.log(`Web Socket: Connection closed`, event);
+    socketStatus.classList.replace("connected", "disconnected");
 
-  if (retryCount < maxRetryCount) {
-    ws = null;
-    reconnectInterval = setInterval(() => {
-      if (retryCount < maxRetryCount) {
-        retryCount++;
-        createWebSocket();
-        console.log(`Web Socket: Attempting Reconnect ${retryCount}`);
-      } else {
-        clearInterval(reconnectInterval);
-      }
-    }, retryInterval);
-  }
+    if (retryCount < maxRetryCount) {
+        ws = null;
+        reconnectInterval = setInterval(() => {
+            if (retryCount < maxRetryCount) {
+                retryCount++;
+                // eslint-disable-next-line no-use-before-define
+                createWebSocket();
+                console.log(`Web Socket: Attempting Reconnect ${retryCount}`);
+            } else {
+                clearInterval(reconnectInterval);
+            }
+        }, retryInterval);
+    }
 }
 
 function onMessageHanlder(event) {
-  const data = JSON.parse(event.data);
+    const data = JSON.parse(event.data);
 
-  if (data.body && typeof data.body === "string") {
-    const result = data.body.match(/!play ([a-f0-9]{32}) ([a-z]{2})/);
-    if (result) {
-      audio.src = `audio/${result[1]}.${result[2]}`;
-      audio.play();
+    if (data.body && typeof data.body === "string") {
+        const result = data.body.match(/!play ([a-f0-9]{32}) ([a-z]{2})/);
+        if (result) {
+            audio.src = `audio/${result[1]}.${result[2]}`;
+            audio.play();
+        }
     }
-  }
 }
 
 function onOpenHandler() {
-  console.log("Web Socket: Connection established");
+    console.log("Web Socket: Connection established");
+    socketStatus.classList.replace("disconnected", "connected");
 
-  retryCount = 0;
-  clearInterval(reconnectInterval);
+    retryCount = 0;
+    clearInterval(reconnectInterval);
 
-  ws.onmessage = onMessageHanlder;
-  ws.onclose = onCloseHanlder;
+    ws.onmessage = onMessageHanlder;
+    ws.onclose = onCloseHanlder;
 
-  ws.send(
-    JSON.stringify({
-      sender: "overlay",
-      body: "Hello from the overlay screen!",
-    })
-  );
+    const message = {
+        sender: "overlay",
+        body: "Hello from the overlay screen!",
+    };
+
+    ws.send(JSON.stringify(message));
 }
 
 function createWebSocket() {
-  ws = new WebSocket("ws://127.0.0.1:8080/");
-  ws.onopen = onOpenHandler;
+    socketStatus.classList.remove("connected");
+    socketStatus.classList.add("disconnected");
+
+    ws = new WebSocket("ws://127.0.0.1:8080/");
+    ws.onopen = onOpenHandler;
 }
 
 createWebSocket();
 
 audio.addEventListener("play", (event) => {
-  notification.style.display = "inline-block";
+    notification.style.display = "inline-block";
 });
 
 audio.addEventListener("ended", (event) => {
-  notification.style.display = "none";
+    notification.style.display = "none";
 });
