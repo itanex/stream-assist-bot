68f5c4fa938b3801fad4132908981de48ee03490
Bryan Wood <itanex@gmail.com>
2024-02-02 11:48:32 -0800

    Create Unit Tests for `DrinkCommmand`

---
+++ bot/commands/drinkCommand.spec.ts
@@ -0,0 +1,64 @@
+// reflect-metadata should be imported
+// before any interface or other imports
+// also it should be imported only once
+// so that a singleton is created.
+import 'reflect-metadata';
+import { ChatClient, ChatUser } from '@twurple/chat';
+import { Container } from 'inversify';
+import winston from 'winston';
+import { mockChatClient, mockLogger } from '../../tests/common.mocks';
+import InjectionTypes from '../../dependency-management/types';
+import { ICommandHandler } from './iCommandHandler';
+import { DrinkCommand } from './drinkCommand';
+
+describe('Drink Command Tests', () => {
+    const container: Container = new Container();
+    let expectedChatClient: ChatClient;
+    let expectedLogger: winston.Logger;
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+        container.unbindAll();
+        container
+            .bind<ChatClient>(ChatClient)
+            .toConstantValue(mockChatClient);
+
+        container
+            .bind<winston.Logger>(InjectionTypes.Logger)
+            .toConstantValue(mockLogger);
+
+        container
+            .bind<ICommandHandler>(InjectionTypes.CommandHandlers)
+            .to(DrinkCommand);
+
+        expectedChatClient = container
+            .get(ChatClient);
+
+        expectedLogger = container
+            .get<winston.Logger>(InjectionTypes.Logger);
+    });
+
+    it('should say something in chat', async () => {
+        // Arrange
+        const channel = 'TestChannel';
+        const command = 'TestCommand';
+        const message = 'TestMessage';
+        const user = <ChatUser>{ displayName: 'TestUser' };
+
+        const subject = container
+            .getAll<ICommandHandler>(InjectionTypes.CommandHandlers)
+            .find(x => x.constructor.name === `${DrinkCommand.name}`);
+
+        // Act
+        await subject.handle(channel, command, user, message, []);
+
+        // Assert
+        expect(expectedChatClient.say)
+            .toHaveBeenCalledTimes(1);
+        expect(expectedChatClient.say)
+            .toHaveBeenCalledWith(channel, expect.anything());
+        expect(expectedLogger.info)
+            .toHaveBeenCalledWith(expect
+                .stringMatching(`(?=.*\\b${command}\\b)(?=.*\\b${channel}\\b)(?=.*\\b${user.displayName}\\b)(?=.*\\b${message}\\b)`));
+    });
+});
