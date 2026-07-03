05f3f1dbb9f11c1ab89006061826ad9ea17bdf0f
Bryan Wood <itanex@gmail.com>
2026-07-03 12:58:29 -0700

    fix(editorconfig): rename editor.config to .editorconfig
    * rename editor.config to .editorconfig - the misnamed file was inert, no editorconfig tooling could recognize it
    * reformat database.config.js and jest.config.js to 4-space/single-quote house style

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
 };
